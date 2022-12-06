c35522cf3c0c835779abedb326bd2904d6f79293
伊藤賢 <wiser@itoukennoMacBook-Air.local>
2022-12-07 06:36:04 +0900

    Initial commit

---
+++ app/src/main/java/com/example/test01/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.test01;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * a.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
