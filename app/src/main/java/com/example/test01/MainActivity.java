965c5fe357d5999ad6ec5a57cb01be701642152e
ito <ito>
2022-12-13 11:38:52 +0900

    #1/Comiit＆Pushテスト

--- app/src/main/java/com/example/test01/MainActivity.java
+++ app/src/main/java/com/example/test01/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        system.out.println("Hello");
+        System.out.println("Helloooo22021213.11.37");
     }
 }
